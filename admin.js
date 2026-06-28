7136e61b492bc227fd3e1a216140710a624f7913
clubedasortees <clubedasortees@gmail.com>
2026-06-28 04:17:30 -0300

    Refactor Firebase integration in admin.js
    Removed Firebase initialization and used existing db reference.

--- admin.js
+++ admin.js
@@ -1,20 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
-  getFirestore
+  collection,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDqWJLQCgt70h3eZXaJjEZWJ0CL7wA8gu4",
-  authDomain: "betstar-premium-v2.firebaseapp.com",
-  projectId: "betstar-premium-v2",
-  storageBucket: "betstar-premium-v2.firebasestorage.app",
-  messagingSenderId: "468025575746",
-  appId: "1:468025575746:web:1fcb558335714f7858e0db",
-  measurementId: "G-J7HZ1JFR55"
+window.onload = async function () {
+
+  const db = window.db;
+
+  const usuarios = await getDocs(collection(db, "usuarios"));
+
+  console.log("Usuários cadastrados:");
+
+  usuarios.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
+
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-console.log("Painel Administrativo conectado!");
